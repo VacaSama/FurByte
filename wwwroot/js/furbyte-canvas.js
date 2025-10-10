94e55b2126d3224ee413b4575f49f31390aa698c
VacaSama <klc_moore@outlook.com>
2025-10-10 16:44:57 -0400

    tesing javascript to see if the canvas is working.  updated the Title for the dashboard. there is still no way to navigate to the dashboard other than typing it into the url. register/login page to be updated.

---
+++ wwwroot/js/furbyte-canvas.js
@@ -0,0 +1,5 @@
+﻿// retrieving the canvas element by its ID from the HTML document
+const canvas = document.getElementById('furbyteCanvas');
+// getting the 2D rendering context for the canvas
+const context = canvas.getContext('2d');
+
