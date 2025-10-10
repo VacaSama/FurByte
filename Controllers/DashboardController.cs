8830e5bb927be95a2ec33e83a38e1a2bcd1f0af7
VacaSama <klc_moore@outlook.com>
2025-10-10 15:01:25 -0400

    Throwing in Bootstrap documentation with no structure. just so that we have them in the dashboard. Restructure will come later, Placeholder Header, and names above progress bars within Card.

---
+++ Controllers/DashboardController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FurByte.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
