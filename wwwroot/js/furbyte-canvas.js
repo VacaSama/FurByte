f5a7769341e6c37b5782f5b6f03cd49bf234b066
VacaSama <klc_moore@outlook.com>
2025-10-12 15:08:52 -0400

    `furbyte-canvas.js` - constant variables added that have been attacched to some of the accessory pieces for a pet. If the accessory kit is unsucessful then i have a back up...unfortunately the backup is hardcoding images of the pets.
    sprites folder has been added to the images folder. it contains `.png`'s of a base model for the cat-pet and a logo designated for the program.
    
    The homepage has changed into a landing page, and the dashboard uses the `furbyte-canvas.js`, still no navigation functions, other than just typing in URL.

--- wwwroot/js/furbyte-canvas.js
+++ wwwroot/js/furbyte-canvas.js
@@ -3,3 +3,10 @@ const canvas = document.getElementById('furbyteCanvas');
 // getting the 2D rendering context for the canvas
 const context = canvas.getContext('2d');
 
+
+// declare a new Image object for the base cat image
+const baseCat = "images/sprites/blankCATBASE.png";
+const baseFace = "images/sprites/baseface.png";
+const eyesClosed = "images/sprites/basefaceclosed.png";
+
+// Load the images on to the canvas 
