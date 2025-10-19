e70c307c6b980e59f80d766889dc8e59454da840
VacaSama <klc_moore@outlook.com>
2025-10-19 17:39:25 -0400

    Outlining dashboard (#20)
    * Throwing in Bootstrap documentation with no structure. just so that we have them in the dashboard. Restructure will come later, Placeholder Header, and names above progress bars within Card.
    
    * Interaction placeholder card and buttons made. Planning Canvas and Sprite structure.  Updated the home page a little and changed it to look more like a landing page.
    
    * A canvas has been made for Ui interaction later on in the dashboard. Instead of making a still shot of the pet profile I want to make it a little more appealing to look at.
    
    * tesing javascript to see if the canvas is working.  updated the Title for the dashboard. there is still no way to navigate to the dashboard other than typing it into the url. register/login page to be updated.
    
    * `furbyte-canvas.js` - constant variables added that have been attacched to some of the accessory pieces for a pet. If the accessory kit is unsucessful then i have a back up...unfortunately the backup is hardcoding images of the pets.
    
    sprites folder has been added to the images folder. it contains `.png`'s of a base model for the cat-pet and a logo designated for the program.
    
    The homepage has changed into a landing page, and the dashboard uses the `furbyte-canvas.js`, still no navigation functions, other than just typing in URL.
    
    * Added the canvas to the css page, a separate one may be made. The canvas color has been changed to a light blue to represent the sky as well as bring visibility to the canvas so that i can mark the area, it was visible for a moment but the background-color doesn't show***
    
    <div> added around the canvas in it's Index HTML file for the dashboard.
    
    * comments added to a few different files : Dashboard Index, site.css, and furbyte-canvas,js
    
    * Removed the individual furbyte base pet models(imgs) and replaced them with a modular sprite sheet that I will later split using a sprite generator. Planning to delete `furbyte-canvas.js` file and place code into the default, `site.js`.
    
    * `furbyte-canvas.js` has been deleted, learning more about JS, modular sprites and adding JS into the `site,js`
    
    Used JS to draw the first row and first column sprite in the modular sprite sheet. this branch can tecnically be closed and once the default layout is updated to include the dashboard we can continue to work on it. Next the register and login pages need to redirect to dashboard as well.

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,59 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+window.onload = function () {
+    // retrieves the canvas element from the HTML document (Dashboard/Index)
+    const canvas = document.getElementById("furbyteCanvas");
+    // retrieves the 2d rendering needed for the canvas 
+    const context = canvas.getContext("2d");
+    // declares and retrieves the modular sprite sheet 
+    const sprite = new Image();
+    // dimensions of each sprite frame (should be..32x32 pixels)
+    const spriteWidth = 250; // some resizing needs to be done.. 
+    const spriteHeight = 250; // sprite sheet is 1024x1536
+    // column index 
+    const spriteX = 0;
+    // row index 
+    const spriteY = 0;
+    // sprite source path 
+    sprite.src = "/images/sprites/modular-cat.png";
+
+    // draw aspecified portion of the sprite sheet onto the canvas
+    sprite.onload = function () {
+
+        context.drawImage(
+            sprite, // what we're drawing
+            spriteX * spriteWidth, spriteY * spriteHeight, // how big and where it is
+            spriteWidth, spriteHeight, 
+            5, 5,
+            spriteWidth, spriteHeight
+        );
+        function drawSprite(spriteX, spriteY) {
+            context.clearRect(0, 0, canvas.width, canvas.height);
+            context.drawImage(
+                sprite,
+                spriteX * spriteWidth, spriteY * spriteHeight,
+                spriteWidth, spriteHeight,
+                0, 0,
+                spriteWidth, spriteHeight
+            );
+        };
+    };
+
+    var gameArea = {
+        canvas: canvas,
+        start: function () {
+            this.canvas.width = 260;
+            this.canvas.height = 260; // may increase to 300
+            this.context = this.canvas.getContext("2d");
+        }
+    };
+
+    function startGame() {
+        gameArea.start();
+        drawSprite(0, 0); // draw first frame
+        drawSprite(0, 3); // draw first frame
+    }
+    startGame();
+};
