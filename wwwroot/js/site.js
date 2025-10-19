935845c1689b8a8c8fd9db929ff0870b4dd37556
VacaSama <klc_moore@outlook.com>
2025-10-19 15:27:22 -0400

    Removed the individiual furbyte base pet models(imgs) and replaced them with amodular sprite sheet that I will later split using a sprite generator. Planning to delete `furbyte-canvas.js` file and place code into the default, `site.js`.

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,23 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+window.onload = function () {
+    const canvas = document.getElementById("furbyteCanvas");
+    const context = canvas.getContext("2d");
+
+    var gameArea = {
+        canvas: canvas,
+        start: function () {
+            this.canvas.width = 480;
+            this.canvas.height = 270; // may increase to 300
+            this.context = this.canvas.getContext("2d");
+        }
+    };
+ 
+    function startGame() {
+        gameArea.start();
+    }
+
+    startGame();
+};
