6b48e1d267a8b8f96ecb222e94b1aac2bb707128
VacaSama <klc_moore@outlook.com>
2025-10-27 16:17:39 -0400

    `MinAgeAttribute.cs` made, it is a class that inherits from `ValidationAttribute` , this is where I will create an age restriction for the user.

---
+++ Models/MinAgeAttribute.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FurByte.Models;
+/// <summary>
+/// The minimum age validation attribute. Inherits from ValidationAttribute,
+/// This specifies the minimum age required for a user.
+/// </summary>
+public class MinAgeAttribute : ValidationAttribute
+{
+}
