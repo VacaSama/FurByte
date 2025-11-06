7b9c3a544087def8b0ad7e29806fb6b995dd3819
VacaSama <klc_moore@outlook.com>
2025-11-06 17:58:11 -0500

    Explict navigation has been added, I want user to user rehoming and minor documentation has been added to the RehomeRequests

--- Models/RehomeRequest.cs
+++ Models/RehomeRequest.cs
@@ -3,6 +3,23 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace FurByte.Models;
 
+/// <summary>
+/// Represents the status of a rehoming request.
+/// </summary>
+/// <remarks>This enumeration is used to indicate the current state of a rehoming process.</remarks>
+public enum RehomeStatus
+{
+	Pending,
+	Approved,
+	Rejected
+}
+
+/// <summary>
+/// Represents a request to rehome a pet, including details about the current and new owners, the pet being rehomed, and
+/// the status of the request.
+/// </summary>
+/// <remarks>This class is used to manage the process of transferring a pet from its current owner to a new owner.
+/// It includes information about the pet, the reason for rehoming, and the current status of the request.</remarks>
 public class RehomeRequest
 {
 	// primary key 
@@ -17,10 +34,13 @@ public class RehomeRequest
 	[Required]
 	public required string OwnerId { get; set; }
 	[Required]
+	// explicit FK navigation to ApplicationUser
+	[ForeignKey("OwnerId")]
 	public required ApplicationUser Owner { get; set; }
 
 	// new owner taking the pet in
 	[Required]
+	[ForeignKey("NewOwnerId")]
 	public required string NewOwnerId { get; set; }
 	[Required]
 	public required ApplicationUser NewOwner { get; set; }
@@ -39,10 +59,5 @@ public class RehomeRequest
 
 	// the different statuses for the rehome request
 	// enum = enumeration, this enumeration defines a set of named constants
-	public enum RehomeStatus
-	{
-		Pending, 
-		Approved, 
-		Rejected
-	}
+
 }
