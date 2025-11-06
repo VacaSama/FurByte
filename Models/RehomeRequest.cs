f7d2a907a8d4fa5ccb74c704109229864eceb2ba
VacaSama <klc_moore@outlook.com>
2025-11-06 17:47:16 -0500

    Most of the data annotations are placed..
    inserted `using System.ComponentModel.DataAnnotations;` into the `RehomeRequest.cs`, my data annotations were missing lol.

--- Models/RehomeRequest.cs
+++ Models/RehomeRequest.cs
@@ -1,30 +1,41 @@
-﻿namespace FurByte.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FurByte.Models;
 
 public class RehomeRequest
 {
 	// primary key 
-	public int RehomeRequestId { get; set; } 
+	[Key]
+	public int RehomeRequestId { get; set; }
 
 	// pet name being rehomed 
+	[Required]
 	public required string PetName { get; set; }
 
 	// the pets current owner 
+	[Required]
 	public required string OwnerId { get; set; }
+	[Required]
 	public required ApplicationUser Owner { get; set; }
 
 	// new owner taking the pet in
+	[Required]
 	public required string NewOwnerId { get; set; }
+	[Required]
 	public required ApplicationUser NewOwner { get; set; }
 
 	// the reason the current owner is rehoming the pet
 	// description field 
+	[Required]
 	public required string ReasonForRehome { get; set; }
 	// ----------------------------Separator---------------------------------
 
 	// created a custom RehomeStatus called status that 
 	// allows our Overseers to approve or reject rehome requests, 
 	// and the users to view the status of their requests.
-	public RehomeStatus Status { get; set; } = RehomeStatus.Pending;
+	[Required]
+	public required RehomeStatus Status { get; set; } = RehomeStatus.Pending;
 
 	// the different statuses for the rehome request
 	// enum = enumeration, this enumeration defines a set of named constants
