d1774472f21ba46c90ec2f99e6391f3b3cb965a6
VacaSama <klc_moore@outlook.com>
2025-11-09 19:43:55 -0500

    The ViewModels folder has two new views that will reference the Products and pets, another ViewModel will have to be created to pair the StorePETViewModel and the StorePRODUCTViewModel (all caps so that you can tell them apart), so generic "store" view model that references both(for clarity).
    A store controller to be made soon so that I can visually see the store. And maybe a special button that takes you there from the pet dashboard.

---
+++ ViewModels/StorePetViewModel.cs
@@ -0,0 +1,24 @@
+﻿namespace FurByte.ViewModels;
+
+/// <summary>
+/// Represents the view model for storing a pet in the Pet Store, users 
+/// can then view and purchase available pets to then add to their collection/ care
+/// </summary>
+public class StorePetViewModel
+{
+	// pet names will be randomly generated
+	// Rudy, Bud, Flower example names 
+	public string? PetName { get; set; }
+	public string? PetType { get; set; } // e.g., "Cat", "Dog", "Dragon"
+
+	// how much the pet costs in pet coins
+	// default for now will be 150 pet coins, 
+	// I don't want to overprice pets yet
+	public int PetFee { get; set; } = 150;
+
+	// is the pet adopted or not, i want to limit how many times you can 
+	// adopt that same pet type. example: yellow cat can only be adopted once
+	// by that user. (cooldown period?)
+	public bool IsAdopted { get; set; }
+
+}
---
+++ ViewModels/StoreProductViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace FurByte.ViewModels;
+
+/// <summary>
+/// This view model stores Store PRODUCT information, for the UI 
+/// base. The user can then purchase items for their pets to improve 
+/// relations between User=>Digital Pet.
+/// </summary>
+public class StoreProductViewModel
+{
+
+}
