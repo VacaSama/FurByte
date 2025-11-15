9d0aefcc2a622d802b80af20ecba2864c45f1db1
VacaSama <klc_moore@outlook.com>
2025-11-15 15:39:28 -0500

    Added the `StorePageViewModel`, it combines both the `StoreProductViewModel` and te `StorePetView.cs`
    The plan is to convert it from a view to a modal-popup

---
+++ ViewModels/StorePageViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace FurByte.ViewModels;
+
+/// <summary>
+/// This view model stores Store PAGE information, for the UI.
+/// It combines both the StorePetViewModel and StoreProductViewModel
+/// </summary>
+public class StorePageViewModel
+{
+
+}
