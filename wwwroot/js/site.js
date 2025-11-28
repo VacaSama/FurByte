2c9ee0d6d2da0d9d0511cf52b3322c967eab487c
VacaSama <klc_moore@outlook.com>
2025-11-27 19:47:45 -0500

    I created a custom enum in the Pet.cs I realized I didn't have a way to cycle through them and I meant to do that earlier.
    XML Documentation added to the Pet Class and PetStats class while I was there,

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -30,6 +30,7 @@ window.onload = function () {
             5, 5,
             spriteWidth, spriteHeight
         );
+        // function to draw a specific sprite frame //
         function drawSprite(spriteX, spriteY) {
             context.clearRect(0, 0, canvas.width, canvas.height);
             context.drawImage(
@@ -42,6 +43,7 @@ window.onload = function () {
         };
     };
 
+    /* Game Area Object */
     var gameArea = {
         canvas: canvas,
         start: function () {
@@ -50,7 +52,7 @@ window.onload = function () {
             this.context = this.canvas.getContext("2d");
         }
     };
-
+    /* When the game starts up the Sprite will load into the game as well*/
     function startGame() {
         gameArea.start();
         drawSprite(0, 0); // draw first frame
@@ -58,6 +60,17 @@ window.onload = function () {
     }
     startGame();
 
+    /* Progress Bar Animation */
+    function animateProgressBars(start) { 
+        // search for the class element -- >
+        let progressBars = document.getElementsByClassName("progress-bar");
+        // this will set the value of each progress bar to the start value
+        let value = start; 
+    }
+
+    /* Loads The Store Partial View , as a pop-up modal
+    - on the pet dashboard page. 
+    */
     function LoadStorePartial() {
         $('#storeContent').html('<p>Loading store...</p>');
         $('#storeModal').modal('show');
